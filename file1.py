6e3529b3c8a1beee5a277b9b5a302d143df065b1
Shpabbiss <69231620+Shpabbiss@users.noreply.github.com>
2021-11-05 13:19:01 +1000

    setup files

---
+++ file1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  5 13:17:51 2021
+
+@author: danbu
+"""
+
+def test(x,y):
+    return x*y
+
---
+++ file2.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  5 13:18:28 2021
+
+@author: danbu
+"""
+
